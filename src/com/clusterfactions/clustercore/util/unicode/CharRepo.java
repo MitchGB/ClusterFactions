5aad9a036654b5c9dc7dddc3d3029a6840b25235
Buby <mitchgertrude222@gmail.com>
2021-02-15 18:35:03 +1100

    Char Repo no longer uses Unicode escapes

--- src/com/clusterfactions/clustercore/util/unicode/CharRepo.java
+++ src/com/clusterfactions/clustercore/util/unicode/CharRepo.java
@@ -1,31 +1,31 @@
 package com.clusterfactions.clustercore.util.unicode;
 
 public class CharRepo {
-	
+
 	//TAGS
-	public static final String RANK_ADMIN_TAG = "\uB001";
+	public static final String RANK_ADMIN_TAG = Character.toString('\uB001');
 	public static final String RANK_MODERATOR_TAG = "";
-	public static final String RANK_PLAYER_TAG = "\uB004";
+	public static final String RANK_PLAYER_TAG = Character.toString('\uB004');
 	
 	
 	public static final String FACTION_LEADER_TAG = "";
 	public static final String FACTION_COLEADER_TAG = "";
 	public static final String FACTION_MODERATOR_TAG = "";
 	
-	public static final String FACTION_CHAT_TAG = "\uC001";
-	public static final String ALLY_CHAT_TAG = "\uC002";
+	public static final String FACTION_CHAT_TAG = Character.toString('\uC001');
+	public static final String ALLY_CHAT_TAG = Character.toString('\uC002');
 	
 	//UI
-	public static final String MENU_CONTAINER = "\uF808"+"\uF001";
-	public static final String TEST_BUTTON = "\uF80C" + "\uF80A" + "\uF002";
+	public static final String MENU_CONTAINER = Character.toString('\uF808') + Character.toString('\uF001');
+	public static final String TEST_BUTTON = Character.toString('\uF80C') + Character.toString('\uF80A') + Character.toString('\uF002');
 	
-	public static final String INSET1 ="\uF801";
-	public static final String INSET2 ="\uF802";
-	public static final String INSET3 ="\uF803";
-	public static final String INSET4 ="\uF804";
-	public static final String INSET5 ="\uF805";
-	public static final String INSET6 ="\uF806";
-	public static final String INSET7 ="\uF807";
-	public static final String INSET8 ="\uF808";
-	public static final String INSET9 ="\uF809";
+	public static final String INSET1 = Character.toString('\uF801');
+	public static final String INSET2 = Character.toString('\uF802');
+	public static final String INSET3 = Character.toString('\uF803');
+	public static final String INSET4 = Character.toString('\uF804');
+	public static final String INSET5 = Character.toString('\uF805');
+	public static final String INSET6 = Character.toString('\uF806');
+	public static final String INSET7 = Character.toString('\uF807');
+	public static final String INSET8 = Character.toString('\uF808');
+	public static final String INSET9 = Character.toString('\uF809');
 }
